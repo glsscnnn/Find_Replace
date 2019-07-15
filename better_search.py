7b39e57d3ecc3c575ef17f26bdbab76cfa1b3871
Logan Lieou <logan.lieou@gmail.com>
2019-07-15 13:26:59 -0500

    documentation
    Read comments for notes on the script

--- better_search.py
+++ better_search.py
@@ -1,12 +1,20 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook #initializes the module load_workbook
 
+#gets the file you are searching must include .xlsx
 file_name = input("Enter file name:  ")
 sheet_name = input("Enter the sheet name:  ")
-values_mode = input("Values mode?  ")
+
+values_mode = input("Values mode?  ") #values mode returns the values in the row no values mode returns the row which something is on
+
+#dictionaries for accepted values
 yes = ['y','yes','YES','Yes','1']
 no = ['no','n','0','No','NO']
+
+#loading the file
 wb = load_workbook(file_name)
 ws = wb[sheet_name]
+
+#non value mode function to return row 
 def nVmode():
     while True:#search function to find the row in which an agreement exists
         search  = str(input("Look up something:  "))
@@ -20,7 +28,7 @@ def nVmode():
                 else:
                     continue
                 continue
-
+#Value mode function to toggle value mode
 def Vmode():
     while True:
         search = str(input("Look up something:  "))
@@ -35,6 +43,7 @@ def Vmode():
                     continue
                 continue
 
+#the real program 
 if values_mode in yes:
     Vmode()
 elif values_mode in no:
